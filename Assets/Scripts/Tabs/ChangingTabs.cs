9b40cb6f646d3a1e0321cdc7d701ff2d0de1236e
Ryneqq <p.rynowiecki@gmail.com>
2017-07-07 09:26:17 +0200

    Zmienianie zakładek działa

---
+++ Assets/Scripts/Tabs/ChangingTabs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangingTabs : MonoBehaviour {
+	private bool ZawodnicyTab = true;
+	private bool KlubyTab = false;
+
+	public GameObject ZawodnicyContent;
+	public GameObject KlubyContent;
+	
+
+	public void ZawodnicyTabClicked(){
+		if(!ZawodnicyTab){
+			ZawodnicyTab = true;
+			KlubyTab = false;
+			ZawodnicyContent.SetActive(true);
+			KlubyContent.SetActive(false);
+		}
+	}
+
+	public void KlubyTabClicked(){
+		if(!KlubyTab){
+			KlubyTab = true;
+			ZawodnicyTab = false;
+			KlubyContent.SetActive(true);
+			ZawodnicyContent.SetActive(false);
+		}
+	}
+}
