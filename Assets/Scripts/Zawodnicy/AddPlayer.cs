1cb4773fd1b5a1cf9d2d00bc207fc4944275c2aa
Ryneqq <p.rynowiecki@gmail.com>
2017-07-07 13:36:55 +0200

    Guzik 'Dodaj zawodnika' dodaje zawodnika z odpowiednim numerem i z odpowiednim położeniem względem wysokości kontentu. Dodano objekt 'Player' którego Liste będe przetrzymywać w programie

---
+++ Assets/Scripts/Zawodnicy/AddPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayer : MonoBehaviour {
+
+	public GameObject playerUI;
+	private List<Player> players;
+	private float y;
+	private int index;
+
+	void Start(){
+		players = new List<Player>();
+		y = gameObject.GetComponent<RectTransform>().rect.height/2 - 50f;
+		index = 1;
+	}
+	public void Clicked(){
+		GameObject temp = Instantiate(playerUI, new Vector3(0f, y, 0f), Quaternion.identity);
+		temp.GetComponent<SetText>().Set(index + ".");
+		temp.transform.SetParent(this.gameObject.transform,false);
+
+
+		Player player = new Player();
+		players.Add(player);
+		y -= 50f;
+		index++;
+	}
+}
